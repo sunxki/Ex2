0b111c2433e186846e26ccb6dc22b6ff337f9ea6
sunxki <dgallegosm1200@alumno.ipn.mx>
2021-10-06 19:17:46 -0700

    Add project files.

---
+++ Ex2/Exercise2.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex2
+{
+    class Exercise2
+    {
+        public int x;
+        public double y;
+        public string z;
+        private string rating;
+        private string palindrome;
+
+
+        //Constructor overloading.
+        public Exercise2(int a, string text, string value)
+        {
+            this.x = a;
+            this.z = text;
+            this.Rating = value;
+        }
+
+        public Exercise2(double a, string text,string value)
+        {
+            this.y = a;
+            this.z = text.ToUpper();
+            this.Palindrome = value;
+        }
+
+        public Exercise2(string text, string text2)
+        {
+            this.z = text + " " + text2;
+        }
+
+        //Method overloading
+        public int multiplicar(int a)
+        {
+            return a * a;
+        }
+        public double multiplicar(double b)
+        {
+            return b * b;
+        }
+
+        // Properties 
+        public string Rating
+        {
+            get { return rating; }
+            set
+            {
+
+                if (value == "A" || value == "B" || value == "C" || value == "D" || value == "F")
+                {
+                    rating = value;
+                }
+                else
+                {
+                    rating = "N/A";
+                }
+            }
+        }
+
+        public string Palindrome
+        {
+            get
+            {
+                return palindrome;
+            }
+
+            set
+            {
+                if (value.SequenceEqual(value.Reverse()))
+                {
+                    palindrome = value;
+                }
+                else
+                {
+                    Console.WriteLine("Not a palindrome");
+                }
+            }
+        }
+
+        //Method to print numbers in diagonal.
+        public void digonal_print()
+        {
+            int i = 0;
+            string space = "";
+            while(i <= 50)
+            {
+                Console.WriteLine(space + i);
+                space = space + " ";
+                i++;
+            }
+            
+        }
+    }
+}
+
---
+++ Ex2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Exercise2 sum1 = new Exercise2(5,"David","C");
+            
+            Console.WriteLine(sum1.x);
+            Console.WriteLine(sum1.z);
+            Console.WriteLine(sum1.multiplicar(sum1.x));
+            Console.WriteLine(sum1.Rating);
+            Console.WriteLine("-----");
+
+            Exercise2 sum2 = new Exercise2(5.1, "Diego","ana");
+            Console.WriteLine(sum2.y);
+            Console.WriteLine(sum2.z);
+            Console.WriteLine(sum2.multiplicar(sum2.y));
+            Console.WriteLine(sum2.Palindrome);
+            Console.WriteLine("-----");
+
+
+            Exercise2 sum2A = new Exercise2(5.1, "Diego", "reconocer");
+            Console.WriteLine(sum2A.y);
+            Console.WriteLine(sum2A.z);
+            Console.WriteLine(sum2A.Palindrome);
+            Console.WriteLine("-----");
+
+            Exercise2 sum3 = new Exercise2("David","Diego");
+            Console.WriteLine(sum3.z.ToUpper());
+            sum3.digonal_print();
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
